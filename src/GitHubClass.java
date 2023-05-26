f7efe35f02d7ae734f7c3ba74082f33f18ecf3dc
TahereNowroozi <you@example.com>
2023-05-25 19:13:41 -0600

    changes from + to -

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -4,8 +4,8 @@ public class GitHubClass {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 10;
-		int c = a+b;
-		System.out.println(c);
+		int c = a-b;
+		System.out.println(c); 
 	}
 
 }
